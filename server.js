18dae1e75420cbf18a3864bb4348c7355ab132d1
mounirzerouali53-rgb <mounirzerouali53@gmail.com>
2026-02-23 07:19:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require("path")
 
 const {
 default: makeWASocket,
-useMultiFileAuthState
+useMultiFileAuthState,
+DisconnectReason
 } = require("@whiskeysockets/baileys")
 
 const app = express()
@@ -13,29 +14,74 @@ app.use(express.static(path.join(__dirname)))
 
 async function getCode(number){
 
-const { state } = await useMultiFileAuthState("./session")
+const { state, saveCreds } = await useMultiFileAuthState("./session")
 
 const sock = makeWASocket({
-logger:P({level:"silent"}),
-auth:state
+logger: P({ level: "silent" }),
+auth: state,
+printQRInTerminal: false
 })
 
-await sock.waitForConnectionUpdate(
-u=>u.connection==="open"
-)
+sock.ev.on("creds.update", saveCreds)
 
-let code=await sock.requestPairingCode(number)
-return code
+await new Promise((resolve,reject)=>{
+
+sock.ev.on("connection.update",(update)=>{
+
+const { connection } = update
+
+if(connection === "connecting"){
+resolve()
 }
+
+if(connection === "close"){
+reject("Connection Closed")
+}
+
+})
+
+})
+
+let code = await sock.requestPairingCode(number)
+return code
+
+}
+
+app.get("/",(req,res)=>{
+res.sendFile(path.join(__dirname,"index.html"))
+})
 
 app.get("/pair",async(req,res)=>{
 
-let number=req.query.number
-if(!number)return res.json({error:"enter number"})
+let number = req.query.number
 
-let code=await getCode(number)
-res.json({code})
+if(!number){
+return res.json({
+status:false,
+msg:"Enter Number"
+})
+}
+
+try{
+
+let code = await getCode(number)
+
+res.json({
+status:true,
+code:code
+})
+
+}catch(e){
+
+res.json({
+status:false,
+msg:e.toString()
+})
+
+}
 
 })
 
-app.listen(3000)
+app.listen(3000,()=>{
+console.log("VENOM PAIR SERVER RUNNING")
+})
